15f2dd44d38d636241cd672c5d98248eb990bd1b
Devikusuma07 <devikusu714@gmail.com>
2026-03-08 13:14:32 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,19 @@
+public class Addition {
+
+    public static void main(String[] args) {
+        int a = 10;
+    int b = 20;
+
+    int sum = a + b;
+    System.out.println("Sum: " + sum);
+
+    int c = 30;
+    int d = 90;
+
+    d += c;
+    System.out.println("updated d: " + d);
+
+        
+    }
+
+}
