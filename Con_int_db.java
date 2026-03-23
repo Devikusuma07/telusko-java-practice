959756f7c540770604e4232fe2ce3f66c3fcfd82
Devikusuma07 <devikusu714@gmail.com>
2026-03-23 12:13:11 +0530

    Add files via upload

---
+++ Con_int_db.java
@@ -0,0 +1,15 @@
+public class Con_int_db {
+    public static void main(String[] args) {
+        int a = 50;
+        double b = a;
+
+        System.out.println("Integer to Double Conversion: " + b);
+
+        double c = 40;
+        int d = (int) c;
+
+        System.out.println("Double to Integer Conversion: " + d);
+
+    }
+    
+}
