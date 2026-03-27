e8b402fd45df4ace2098b3d15c47bc6d63aca856
Devikusuma07 <devikusu714@gmail.com>
2026-03-27 19:53:42 +0530

    Add files via upload

---
+++ Error.java
@@ -0,0 +1,17 @@
+//Identify and fix syntax errors:
+
+/*class Test
+{
+  public static void main(String args)
+  {
+    System.out.println("Java");
+  }
+}*/
+
+class Error
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Java");
+  }
+}
