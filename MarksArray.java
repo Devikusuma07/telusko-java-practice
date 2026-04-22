a2efd5f59b2f2ca4918c7cdcadfcacc5fd61ba61
Devikusuma07 <devikusu714@gmail.com>
2026-04-22 10:33:47 +0530

    Add files via upload

---
+++ MarksArray.java
@@ -0,0 +1,11 @@
+public class MarksArray {
+    public static void main(String[] args) {
+        int[] arr = {98, 67, 45, 67, 98};
+
+        for(int i = 0; i <arr.length; i++) {
+            System.out.println(arr[i]);
+
+        }
+    }
+    
+}
