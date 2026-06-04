a764864f08edb18d018d09d2d49d6fcb6b142403
Devikusuma07 <devikusu714@gmail.com>
2026-06-04 11:36:27 +0530

    Add files via upload

---
+++ nameageClg.java
@@ -0,0 +1,12 @@
+public class nameageClg {
+    public static void main(String[] args) {
+        String name = "Kusuma";
+        int age = 30;
+        String college = "ABC University";
+
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+        System.out.println("College:" + college);
+    }
+    
+}
