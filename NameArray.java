d3ef3540abc3a881b4fd870224ce3a678efad6b1
Devikusuma07 <devikusu714@gmail.com>
2026-06-05 12:07:51 +0530

    Add files via upload

---
+++ NameArray.java
@@ -0,0 +1,10 @@
+public class NameArray {
+    public static void main(String[] args) {
+        String[] names = {"devi", "kusuma", "sri", "lakshmi", "priya"};
+
+        for(int i = 0 ; i < names.length;i++) {
+            System.out.println(names[i]);
+        }
+    }
+    
+}
