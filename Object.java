9102c0c58ce1cd7ee7cecf8725a62b58cbccfcd4
Devikusuma07 <devikusu714@gmail.com>
2026-06-06 11:58:34 +0530

    Add files via upload

---
+++ Object.java
@@ -0,0 +1,18 @@
+class Police 
+{
+    void display() 
+    {
+        System.out.println(" Hello! I am police");
+    }
+}
+
+public class Object {
+
+    public static void main(String[] args) {
+         Police p1 = new Police();
+        p1.display();
+    
+        
+    }
+}
+
