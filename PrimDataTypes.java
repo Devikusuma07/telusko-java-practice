b565479147651db212025a626205243197adbe1c
Devikusuma07 <devikusu714@gmail.com>
2026-06-10 10:01:45 +0530

    Add files via upload

---
+++ PrimDataTypes.java
@@ -0,0 +1,23 @@
+public class PrimDataTypes {
+    public static void main(String[] args) {
+        byte by = 100;
+        short sh = 10000;
+        int in = 1000000;
+        long lo = 10000000000L;
+        float fl = 0.25f;
+        double db = 0.8645637937;
+        char ch = 'A';
+        boolean bl = true;
+
+        System.err.println("Byte value: " + by);
+        System.err.println("Short value: " + sh);
+        System.err.println("Int value: " + in);
+        System.err.println("Long value: " + lo);
+        System.err.println("Float value: " + fl);
+        System.err.println("Double value: " + db);
+        System.err.println("Char value: " + ch);
+        System.err.println("Boolean value: " + bl);
+
+    }
+    
+}
