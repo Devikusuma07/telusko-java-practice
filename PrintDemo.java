2442d59c19a60addf49f1fe735dc90b8e8515250
Devikusuma07 <devikusu714@gmail.com>
2026-06-11 11:41:28 +0530

    Add files via upload

---
+++ PrintDemo.java
@@ -0,0 +1,21 @@
+/*Write a program that shows the difference between:
+
+println()
+
+print()*/
+
+ 
+public class PrintDemo {
+    public static void main(String[] args) {
+        int i = 5;
+        int y = 7;
+        System.out.println("print the value i in the same line:");
+        System.out.print(i + " ");
+        System.out.print(y);
+        System.out.println(" ");
+        System.out.println("print the value i in the next line ");
+        System.out.println(i);
+        System.out.println(y);
+    }
+    
+}
