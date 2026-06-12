dd540e1dbd145009aabf376b3eaf0c96aca237d7
Devikusuma07 <devikusu714@gmail.com>
2026-06-12 09:14:33 +0530

    Add files via upload

---
+++ quoRem.java
@@ -0,0 +1,14 @@
+public class quoRem {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 5;
+
+        float quotient = (float) a / b;
+        int remainder = a % b;
+
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+
+    }
+    
+}
