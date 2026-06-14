91871b29bd5cc88dc62b72df603b3c8964227acd
Devikusuma07 <devikusu714@gmail.com>
2026-06-14 08:37:53 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,11 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int num[] = { 4, 7, 8, 4, 6};
+
+        for(int i = num.length - 1; i >= 0; i--)
+        {
+            System.out.println(num[i]);
+        }
+    }
+    
+}
