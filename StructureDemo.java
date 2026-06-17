3694edc955f1a0b8b710c674aaa2b0fdc73f95c7
Devikusuma07 <devikusu714@gmail.com>
2026-06-17 10:43:07 +0530

    Add files via upload

---
+++ StructureDemo.java
@@ -0,0 +1,11 @@
+//Purpose: To show the structure of a Java program
+
+class StructureDemo { // class declartion
+
+    public static void main(String[] args) { //main method
+
+        System.out.println("This program shows the structure of a Java program"); //statement inside the main method
+
+    }
+    
+}
