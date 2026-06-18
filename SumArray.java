3987c0041912401dfcef50f34da2b2397cc247de
Devikusuma07 <devikusu714@gmail.com>
2026-06-18 16:58:57 +0530

    Add files via upload

---
+++ SumArray.java
@@ -0,0 +1,14 @@
+public class SumArray {
+    public static void main(String[] args) {
+        int num[] = {45, 67, 86, 88};
+
+        int sum = 0;
+        for(int i = 0 ; i < num.length; i++) {
+            sum += num[i];
+            System.out.println(sum);
+
+        }
+
+    }
+    
+}
