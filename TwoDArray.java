005c116c4700ae409dee8501ef81b32c6eca7516
Devikusuma07 <devikusu714@gmail.com>
2026-06-24 19:21:04 +0530

    Add files via upload

---
+++ TwoDArray.java
@@ -0,0 +1,14 @@
+public class TwoDArray {
+    public static void main(String[] args) {
+        int[][] num = {{1,2} , {4,5} , {7,8}};    
+
+        for(int i = 0; i<num.length; i++){
+            for(int j = 0; j<num[i].length; j++){
+                System.out.print(num[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+    
+}
