b84927e12ce30ff922c7ee96c40c2dc91ff4dff9
Devikusuma07 <devikusu714@gmail.com>
2026-06-28 21:09:48 +0530

    Add files via upload

---
+++ EnhancedForLoop.java
@@ -0,0 +1,10 @@
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 57, 48};
+
+        for(int num : nums) {
+            System.out.println("" + num);
+        }
+    }
+    
+}
