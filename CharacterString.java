497bebde48c8626a66f8769499eea2c63cd6c696
Devikusuma07 <devikusu714@gmail.com>
2026-07-04 14:21:56 +0530

    Add files via upload

---
+++ CharacterString.java
@@ -0,0 +1,13 @@
+public class CharacterString {
+    public static void main(String[] args) {
+        String str = "Kusuma";
+
+        for(int i = 0 ; i <str.length(); i++) {
+            char ch = str.charAt(i);
+            System.out.println("character at index " + i + "is :" + ch);
+
+        }
+    }
+}
+
+
