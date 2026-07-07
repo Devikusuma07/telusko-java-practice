c7190b990f40e39b08e9b5c3eb5d89b016b1cc1f
Devikusuma07 <devikusu714@gmail.com>
2026-07-08 05:28:19 +0530

    Add files via upload

---
+++ EnhancedLoopString.java
@@ -0,0 +1,10 @@
+public class EnhancedLoopString {
+    public static void main(String[] args) {
+        String[] name = {"kusuma", "Devi", "siri", "pooja", "lucky"};
+
+        for(String id : name) {
+            System.out.println("name : " + id);
+        }
+    }
+    
+}
