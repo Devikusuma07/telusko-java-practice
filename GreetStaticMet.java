24c05f5135689522a51e5b0ab49aa1aec8046df5
Devikusuma07 <devikusu714@gmail.com>
2026-07-09 19:57:43 +0530

    Add files via upload

---
+++ GreetStaticMet.java
@@ -0,0 +1,12 @@
+class Greet{
+    static void greet(){
+        System.out.println("Hello, welcome to java!");
+    }
+}
+
+public class GreetStaticMet {
+    public static void main(String[] args) {
+        Greet.greet();
+    }
+    
+}
