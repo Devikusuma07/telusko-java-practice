8ba71ef52ae075687ed289378a455a12aead82ea
Devikusuma07 <devikusu714@gmail.com>
2026-07-09 19:57:25 +0530

    Add files via upload

---
+++ FrequencyChar.java
@@ -0,0 +1,16 @@
+public class FrequencyChar {
+    public static void main(String[] args) {
+        String str = "kusuma";
+        int[] freq = new int[256];
+
+        for(int i = 0; i< str.length(); i++){
+            freq[str.charAt(i)]++;
+        }
+        for(int i = 0; i< str.length(); i++) {
+            if (freq[str.charAt(i)] != 0) {
+                System.out.println(str.charAt(i) + " = " + freq[str.charAt(i)]);
+                freq[str.charAt(i)] = 0;
+            }
+        }
+    }
+}
