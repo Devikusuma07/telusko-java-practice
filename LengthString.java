8d466516db24c6ff74962495680aa818aaee878c
Devikusuma07 <devikusu714@gmail.com>
2026-07-10 11:52:54 +0530

    Add files via upload

---
+++ LengthString.java
@@ -0,0 +1,8 @@
+public class LengthString {
+    public static void main(String[] args) {
+        String str = "Kusuma";
+        int length = str.length();
+        System.out.println("Length of the string: " + length);
+    }
+    
+}
