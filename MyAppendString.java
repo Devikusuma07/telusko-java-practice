be8bfc0d0718ad20ff7575d55b719280ba712546
Devikusuma07 <devikusu714@gmail.com>
2026-07-12 13:52:30 +0530

    Add files via upload

---
+++ MyAppendString.java
@@ -0,0 +1,12 @@
+public class MyAppendString {
+    public static void main(String[] args) {
+        String s1 = "Devi";
+        s1 = s1 +  " Kusuma";
+
+        System.out.println(s1);
+
+
+        
+    }
+    
+}
