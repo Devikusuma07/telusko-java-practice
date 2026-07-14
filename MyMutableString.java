2701549dcf3dca32b305bb9b33a05234e8578311
Devikusuma07 <devikusu714@gmail.com>
2026-07-14 15:00:22 +0530

    Add files via upload

---
+++ MyMutableString.java
@@ -0,0 +1,15 @@
+public class MyMutableString {
+    
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(s.hashCode());
+        //StringBuilder sb = new StringBuilder("Java");
+        //sb.append(" Programming");
+        
+        
+    
+    }
+    
+}
+
+    
