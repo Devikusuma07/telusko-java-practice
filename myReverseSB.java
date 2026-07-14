af876295907b64afc0be7e7cf9a1013f73ed0441
Devikusuma07 <devikusu714@gmail.com>
2026-07-15 00:12:40 +0530

    Add files via upload

---
+++ myReverseSB.java
@@ -0,0 +1,11 @@
+public class myReverseSB {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Java is easy to learn");
+        sb.replace(8, 12, "hard");
+
+        System.out.println(sb);
+
+
+    }
+    
+}
