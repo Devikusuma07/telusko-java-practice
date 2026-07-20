bbfcef70985b875dae0a23178afcbb56943e4a03
Devikusuma07 <devikusu714@gmail.com>
2026-07-20 12:16:13 +0530

    Add files via upload

---
+++ SquareStaticMet.java
@@ -0,0 +1,12 @@
+class Square{
+    public static int square(int n) {
+        return n * n;
+    }
+}
+
+public class SquareStaticMet {
+    public static void main(String[] args) {
+        System.out.println("THE SQUARE NUMBER: " + Square.square(9)); 
+    }
+    
+}
