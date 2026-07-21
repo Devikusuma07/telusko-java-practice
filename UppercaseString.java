1ca22c1365c63d2cdbfb557f5550c9c359dcca0f
Devikusuma07 <devikusu714@gmail.com>
2026-07-21 15:17:32 +0530

    Add files via upload

---
+++ UppercaseString.java
@@ -0,0 +1,8 @@
+public class UppercaseString {
+    public static void main(String[] args) {
+        String str = "Kusuma";
+        String U = str.toUpperCase();
+        System.out.println("Uppercase String: " + U);
+    }
+    
+}
